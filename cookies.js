fc913e120628f5e8716b2861736ea27e8bdf1b2b
OlejukM <olejukmaslo@gmail.com>
2018-06-21 15:57:46 +0300

    done

--- cookies.js
+++
@@ -1,9 +0,0 @@
-function setCookie( name, value, expires, path, domain, secure ) {
-	document.cookie = name + "=" + escape(value) +
-		((expires)  ?  "; expires=" + expires.toGMTString():   "") + 
-		((path)  ?  "; path=" + path   :   "")   + 
-		((domain)  ?  "; domain="   +  domain :   "")   + 
-		((secure)  ?  ";   secure"  :   "");
-}
-
-setCookie( 'option', id_option, new Date( (new Date()).getTime() + ( 60 * 60 * 24*24  ) ), '/' );
